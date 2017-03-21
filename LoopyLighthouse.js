a558631d830f38d1703126e0e5fc2bd6e470b589
Fayezo <fayezosaadi@gmail.com>
2017-03-21 00:51:41 +0000

    Loopy Lighthouse 2 (Stretch)

--- LoopyLighthouse.js
+++ LoopyLighthouse.js
@@ -1,6 +1,23 @@
+//Loopy Lighthouse2 Assignment
+
+function loopyLighthouse(range, multiples, words){
+  for (var i = range[0]; i <= range[1]; i++) {
+    if((i % multiples[0] === 0) && (i % multiples[1] === 0))
+    { console.log(words[0] + words[1]); }
+    else if(i % multiples[0] === 0)
+    { console.log(words[0]); }
+    else if(i % multiples[1] === 0)
+    { console.log(words[1]); }
+    else { console.log(i); }
+  }
+}
+loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
+
+
+
 //Loopy Lighthouse Assignment
 //Sloutuion 1
-var numbers= 100;
+/*var numbers= 100;
 for(var i=0; i<=100; i++)
 {
 if(numbers % 3 === 0 && numbers % 4 === 0)
@@ -11,7 +28,7 @@ else if(numbers % 4 === 0)
   console.log("Lighthouse");
 else {console.log(numbers);}
 numbers+=1
-}
+}*/
 
 
 //Sloutuion 1
